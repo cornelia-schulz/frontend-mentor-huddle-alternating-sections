29b317ceb24def97c3510fd66997393f2f9c3a23
cornelia-schulz <cornelia.schulz.nz@gmail.com.com>
2019-01-22 00:18:01 +1300

    added babel

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,13 +1,11 @@
 'use strict'
 
+const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: [
-    './src/app.js'
-  ],
 
   module: {
     rules: [
@@ -22,11 +20,16 @@ module.exports = {
           'css-loader',
           'sass-loader'
         ]
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
       }
     ]
   },
 
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
         filename: 'index.html',
