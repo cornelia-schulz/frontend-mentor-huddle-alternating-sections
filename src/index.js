51204cab9e8d530af1447df36f1b1e8356092fdd
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-04-19 12:39:22 +1000

    setting up i18next and @panter/vue-i18next

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
+import i18next from 'i18next'
+import VueI18Next from '@panter/vue-i18next'
 import App from './App.vue'
+
+Vue.use(VueI18Next)
+
+i18next.init({
+  lng: 'de',
+  resources: {
+
+  }
+})
+
+const i18n = new VueI18Next(i18next)
 
 if (document.getElementById('app')) {
   /* eslint-disable-next-line no-new */
   new Vue({
     el: '#app',
+    i18n: i18n,
     render: h => h(App)
   })
 }
